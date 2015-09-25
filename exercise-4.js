9aaa0b013fea116be34c1b315820524c3f6c5b9b
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-25 00:39:27 +0000

    Created exercise-4.js and updated package.json with colors

---
+++ exercise-4.js
@@ -0,0 +1,54 @@
+var Promise = require('bluebird');
+var colors = require('colors');
+var request = Promise.promisify(require('request'));
+var prompt = require('prompt');
+Promise.promisifyAll(prompt);
+
+prompt.start();
+
+prompt.getAsync(['englishWord']).then( function (result) {
+    
+    return result.englishWord;
+        
+}).then( function(userWord){
+    
+    return request('http://words.bighugelabs.com/api/2/11c036dfbb595f828a10c807a507e679/' + userWord+"/json");
+        
+        
+}).spread( function(res, body) {
+    
+    var format = JSON.parse(body);
+    return format;
+
+}).catch( function(err) {        
+        
+    var superError = "####### This word is invalid! " .random + "This word is invalid! " .random + "This word is invalid! " .random + "This word is invalid! " .random + "This word is invalid! " .random + "This word is invalid! " .random + "This word is invalid! " .random + "This word is invalid! " .random + "#######" .random;
+    
+    console.log(superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+
+                superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+
+                superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+
+                superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+
+                superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+
+                superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+
+                superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+
+                superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError+"\n"+superError);   
+    return err;
+
+}).then( function(formatObject) {
+    
+    if(formatObject.noun !== undefined){
+        
+        var nounSyn = formatObject.noun.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", ");
+        console.log("\nNoun synonyms:\n" .black.bgYellow +nounSyn .rainbow.bgWhite +".\n" .rainbow.bgWhite);
+    }
+    if(formatObject.verb !== undefined){
+        
+        var verbSyn = formatObject.verb.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", ");
+        console.log("Verb synonyms:\n" .black.bgYellow +verbSyn .rainbow.bgWhite +".\n" .rainbow.bgWhite);
+    }
+    if(formatObject.adjective !== undefined){
+        
+        var adjSyn = formatObject.adjective.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", ");
+        console.log("Adjective synonyms:\n" .black.bgYellow +adjSyn .rainbow.bgWhite +".\n" .rainbow.bgWhite);
+    }
+});
