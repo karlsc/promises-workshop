c7421d154c438860161ccc6431c6c9f937f9b9d8
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-27 18:37:57 +0000

    Changed the  to make it a promise

--- exercise-6.js
+++ exercise-6.js
@@ -16,46 +16,50 @@ function letsHaveFun(){
                 var words = JSON.parse(body);
                 return words;
             
-        }).then( function(result){
-                
-            result.map( function(x){
+        }).map( function(x){
             
-                return request('http://words.bighugelabs.com/api/2/11c036dfbb595f828a10c807a507e679/'+x.word+"/json").spread(
+            return request('http://words.bighugelabs.com/api/2/11c036dfbb595f828a10c807a507e679/'+x.word+"/json").spread(
                     
-                    function(res, body) {
+                function(res, body) {
                         
-                        var format = JSON.parse(body);
-                        var table = new Table({
-                          chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
-                                 , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
-                                 , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
-                                 , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
-                        });
-                         
-                        table.push( ["Random word:" .bgYellow.black,x.word[0].toUpperCase()+x.word.slice(1) .trap] );
+                    var format = JSON.parse(body);
+                    return {word: x.word, response: format};
+                }
+            );    
+        }).then( function(results){   
+                   
+            var table = new Table({
+            chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+                   , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+                   , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+                   , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+            });
+            
+            for(var i = 0 ; i < 2 ; i++){
+                
+                table.push( ["Random word:" .bgYellow.black,results[i].word.charAt(0).toUpperCase()+results[i].word.slice(1) .trap] );
 
-                        if(format.noun !== undefined){
-                        
-                            var nounSyn = format.noun.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", \n");
-                            table.push( ["Noun synonyms:" .bgRed.black, nounSyn] );
-                        }
-                        if(format.verb !== undefined){
-                            
-                            var verbSyn = format.verb.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", \n");
-                            table.push( ["Verb synonyms:" .bgGreen.black, verbSyn] );
-                        }
-                        if(format.adjective !== undefined){
-                            
-                             var adjSyn = format.adjective.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", \n");
-                             table.push( ["Adjective synonyms:" .bgBlue.black, adjSyn] );
-                        }
-                        console.log(table.toString());
-                    }
-                ).catch( function(err) {
+                if(results[i].response.noun !== undefined){
+                
+                    var nounSyn = results[i].response.noun.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", \n");
+                    table.push( ["Noun synonyms:" .bgRed.black, nounSyn] );
+                }
+                if(results[i].response.verb !== undefined){
                     
-                    return err;
-                });
-            }); 
+                    var verbSyn = results[i].response.verb.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", \n");
+                    table.push( ["Verb synonyms:" .bgGreen.black, verbSyn] );
+                }
+                if(results[i].response.adjective !== undefined){
+                    
+                     var adjSyn = results[i].response.adjective.syn.map(function(x){return x[0].toUpperCase() + x.slice(1);}).join(", \n");
+                     table.push( ["Adjective synonyms:" .bgBlue.black, adjSyn] );
+                }
+            }
+            console.log(table.toString());
+            
+        }).catch( function(err) {
+            
+            return err;
         });
     });
 }
